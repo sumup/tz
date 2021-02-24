48ea572e4c2d357856a3cf0ae464c623e736dde7
Andrey Chernykh <andrei.chernykh@gmail.com>
2021-02-24 21:26:50 +0100

    Offline mode (updating from an tar.gz archive placed in /priv/tzdata_archives)

---
+++ config/config.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :tz, offline_mode: true, archive_path: Path.relative("../priv/tzdata_archives")
--- lib/updater.ex
+++ lib/updater.ex
@@ -23,45 +23,92 @@ if Code.ensure_loaded?(Mint.HTTP) do
       case HTTPClient.request("GET", "/time-zones/tzdb/version", hostname: "data.iana.org") do
         %HTTPResponse{body: body, status_code: 200} ->
           {:ok, body |> List.first() |> String.trim()}
+
         _ ->
           :error
       end
     end
 
+    defp is_offline?, do: Application.fetch_env!(:tz, :offline_mode) == true
+
     defp maybe_update_tz_database() do
+      if is_offline?() do
+        maybe_update_from_archive()
+      else
+        maybe_update_from_iana()
+      end
+    end
+
+    defp maybe_update_from_iana do
       case fetch_iana_tz_version() do
         {:ok, latest_version} ->
           if latest_version != PeriodsProvider.version() do
-            case update_tz_database(latest_version) do
+            case update_tz_database_from_iana(latest_version) do
               :ok ->
                 delete_tz_database(PeriodsProvider.version())
                 :updated
-              _ -> :error
+
+              _ ->
+                :error
             end
           end
+
         :error ->
           Logger.error("Tz failed to read the latest version of the IANA time zone database")
           :no_update
       end
     end
 
-    defp update_tz_database(version) do
+    defp maybe_update_from_archive do
+      archive_path = Application.fetch_env!(:tz, :archive_path)
+
+      latest_archive_name =
+        archive_path
+        |> File.ls!()
+        |> Enum.sort()
+        |> List.last()
+
+      latest_version = String.trim(latest_archive_name, "tzdata")
+
+      if latest_version != PeriodsProvider.version() do
+        latest_archive = [archive_path, latest_archive_name] |> Path.join() |> File.read!()
+
+        case extract_tz_database(latest_version, latest_archive) do
+          :ok ->
+            delete_tz_database(PeriodsProvider.version())
+            :updated
+
+          _ ->
+            :error
+        end
+      end
+    end
+
+    defp update_tz_database_from_iana(version) do
       case download_tz_database(version) do
         {:ok, content} ->
           extract_tz_database(version, content)
           :ok
+
         :error ->
-          Logger.error("Tz failed to download the latest archived IANA time zone database (version #{version})")
+          Logger.error(
+            "Tz failed to download the latest archived IANA time zone database (version #{version})"
+          )
+
           :error
       end
     end
 
     defp download_tz_database(version) do
       Logger.info("Tz is downloading the latest IANA time zone database (version #{version})...")
-      case HTTPClient.request("GET", "/time-zones/releases/tzdata#{version}.tar.gz", hostname: "data.iana.org") do
+
+      case HTTPClient.request("GET", "/time-zones/releases/tzdata#{version}.tar.gz",
+             hostname: "data.iana.org"
+           ) do
         %HTTPResponse{body: body, status_code: 200} ->
           Logger.info("Tz download done")
           {:ok, body}
+
         _ ->
           :error
       end
@@ -85,11 +132,13 @@ if Code.ensure_loaded?(Mint.HTTP) do
         'iso3166.tab',
         'zone1970.tab'
       ]
-      :ok = :erl_tar.extract(tmp_archive_path, [
-        :compressed,
-        {:cwd, Path.join(:code.priv_dir(:tz), tz_data_dir)},
-        {:files, files_to_extract}
-      ])
+
+      :ok =
+        :erl_tar.extract(tmp_archive_path, [
+          :compressed,
+          {:cwd, Path.join(:code.priv_dir(:tz), tz_data_dir)},
+          {:files, files_to_extract}
+        ])
 
       :ok = File.rm!(tmp_archive_path)
     end
